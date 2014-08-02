4c22c2e0d6dfa70a0fd69c5891e29d66345eef09
Wes Mason <wes@1stvamp.org>
2014-08-02 16:15:17 +0100

    Fully working CLI app to capture button

--- dramadive/button_app.py
+++ dramadive/button_app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+from select import select
 from sys import exit, stderr
 from time import sleep
 
@@ -9,31 +11,39 @@ _bytes[7] = 0x02
 WRITE_STRING = "".join(map(chr, _bytes))
 CLOSED = 0x15
 OPEN = 0x17
-DEPRESSED = 0x16
+DOWN = 0x16
 
 def main():
-    fd = open('/dev/big_red_button', 'rb+')
+    fd_id = os.open('/dev/big_red_button', os.O_RDWR|os.O_NONBLOCK)
+    fd = os.fdopen(fd_id, 'rb+')
 
     last = None
+    return_code = 0
     try:
         while True:
-            print('.')
             data = fd.write(bytes(_bytes))
             data = fd.read(8)
-            if data is not None:
-                if data[0] != last and data[0] == CLOSED:
+            if data is not None and data[0] != last:
+                if data[0] == CLOSED:
                     print('closed')
-                elif data[0] != last and data[0] == OPEN:
+                elif last == DOWN and data[0] == OPEN:
+                    print('up')
+                elif data[0] == OPEN:
                     print('open')
-                elif data[0] == DEPRESSED:
+                elif data[0] == DOWN:
                     print('down')
-            last = data[0]
+                last = data[0]
             sleep(0.1)
+
     except KeyboardInterrupt:
-        return 0
+        return_code = 0
     except Exception as e:
         print(e, file=stderr)
-        return 1
+        return_code = 1
+    finally:
+        fd.close()
+
+    return return_code
 
 
 if __name__ == '__main__':
